0cf3e9ae3bb10c4de30ff53aca5944cf652346f0
Sakamoto0525 <525masaya@gmail.com>
2022-10-01 16:24:39 +0900

    Controller作成

---
+++ laravel/app/Http/Controllers/UsersController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index(): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function create(): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function store(Request $request): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function show(int $id): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function edit(int $id): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function update(Request $request, int $id): \Illuminate\Http\Response
+    {
+        //
+    }
+
+    public function destroy(int $id): \Illuminate\Http\Response
+    {
+        //
+    }
+}
