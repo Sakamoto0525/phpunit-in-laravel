8c199a8f8b549d99b54192eed61c0b6f7b644856
Sakamoto0525 <525masaya@gmail.com>
2022-10-02 17:41:05 +0900

    Requestとバリデーションを作成

--- laravel/app/Http/Controllers/UsersController.php
+++ laravel/app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\Api\UserCreateRequest;
 
 class UsersController extends Controller
 {
@@ -23,7 +24,7 @@ class UsersController extends Controller
         return response()->json($user, 200);
     }
 
-    public function create(Request $request)
+    public function create(UserCreateRequest $request)
     {
         $user = new User;
         $user->name = $request->input('name');
