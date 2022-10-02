d717173975c38eff4f32709ced16e4793474c9c7
Sakamoto0525 <525masaya@gmail.com>
2022-10-02 15:53:07 +0900

    API作成

--- laravel/app/Http/Controllers/UsersController.php
+++ laravel/app/Http/Controllers/UsersController.php
@@ -10,39 +10,48 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::all();
-
         if ($users->isEmpty()) return response()->json(null, 404);
 
         return response()->json(User::all(), 200);
     }
 
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
     public function show(int $id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        return response()->json($user, 200);
     }
 
-    public function edit(int $id)
+    public function create(Request $request)
     {
-        //
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->save();
+
+        return response()->json(null, 201);
     }
 
-    public function update(Request $request, int $id): \Illuminate\Http\Response
+    public function update(Request $request, int $id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        $user->name = $request->input('name');
+        $user->password = $request->input('password');
+        $user->save();
+
+        return response()->json(null, 204);
     }
 
-    public function destroy(int $id): \Illuminate\Http\Response
+    public function delete(int $id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) return response()->json(null, 404);
+
+        User::find($id)->delete();
+        return response()->json(null, 204);
     }
 }
