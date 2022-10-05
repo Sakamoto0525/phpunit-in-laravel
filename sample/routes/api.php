c83152d75750de40b61ff71851abba51014ac136
Sakamoto0525 <525masaya@gmail.com>
2022-10-05 22:23:23 +0900

    Users API作成

--- sample/routes/api.php
+++ sample/routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/users', 'UsersController@index');
+Route::get('users/{id}', 'UsersController@show');
+Route::post('users', 'UsersController@create');
+Route::put('users/{id}', 'UsersController@update');
+Route::delete('users/{id}', 'UsersController@delete');
